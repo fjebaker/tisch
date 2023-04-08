beb857d1ff0e07f2f7e7fba1d062ab14ec4e72ed
fjebaker <fergusbkr@gmail.com>
2023-04-08 16:23:11 +0100

    feat: add module to build.zig
    This was missing the `b.addModule` to make it available downstream.

--- build.zig
+++ build.zig
@@ -15,19 +15,10 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zig-tables",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
+    _ = b.addModule("tisch", .{
+        .source_file = .{ .path = "src/main.zig" },
+        .dependencies = &.{},
     });
-
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    lib.install();
 
     // Creates a step for unit testing.
     const main_tests = b.addTest(.{
